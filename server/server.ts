0f5f33f3589945eac9335784f79dc1a7a0de64c1
Logan Walker <logantwalker95@gmail.com>
2023-06-04 19:36:42 -0400

    will this make the thing run?

--- server/server.ts
+++ server/server.ts
@@ -17,15 +17,10 @@ app.use(cors());
 
 app.use(routes);
 
-// ...
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
-// ...
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`port: ${port}`);
